0bbdc23c4092e9e3ff084d8da285b70ebe1d9884
Ivan Kozak <2.vanya.k@gmail.com>
2022-08-19 01:29:44 +0300

    Add Setup class

---
+++ TipCalc.WPF/Setup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+using MvvmCross.Platforms.Wpf.Core;
+using Serilog;
+using Serilog.Extensions.Logging;
+
+namespace TipCalc.WPF
+{
+    public class Setup : MvxWpfSetup<Core.App>
+    {
+        protected override ILoggerFactory? CreateLogFactory()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .CreateLogger();
+            return new SerilogLoggerFactory();
+        }
+
+        protected override ILoggerProvider? CreateLogProvider()
+        {
+            return new SerilogLoggerProvider();
+        }
+    }
+}
